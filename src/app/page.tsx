5042acdc77eec7e3135efe69f5fb939c3d667253
Hakkan <hakkanparbej@gmail.com>
2025-10-13 21:49:21 +0000

    It should be placed in the bottom of the page (_for element footer_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import Calculator from '@/components/calculator';
 
 export default function Home() {
   return (
-    <main className="flex min-h-dvh flex-col items-center justify-center bg-background p-4 antialiased">
+    <main className="flex min-h-dvh flex-col items-center justify-between bg-background p-4 antialiased">
+      <div />
       <div className="w-full max-w-sm">
         <Calculator />
       </div>
-      <footer className="absolute bottom-4 text-center text-sm text-muted-foreground">
+      <footer className="w-full text-center text-sm text-muted-foreground">
         If you don't like it just hate him{' '}
         <a
           href="https://hakkan.is-a.dev/"
